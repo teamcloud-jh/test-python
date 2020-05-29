81f07c38a72d528b2e95659adea46dd5d5aca320
teamcloud-jh <66046114+teamcloud-jh@users.noreply.github.com>
2020-05-29 18:16:33 +0800

    Create test-api.py

---
+++ test-api.py
@@ -0,0 +1,25 @@
+import v20
+
+#
+# Create the API context based on the provided arguments
+#
+###################################
+# Error: Inssuficient authorisation
+api = v20.Context(
+    hostname='api-fxpractice.oanda.com',
+    token='43b546aff89ed477d1c2841a0193b263-23e07b9e94639df4e20d217b02ddaa97'
+)
+
+#
+# Submit the request to create the Market Order
+#
+response = api.order.market(
+    accountID='101-011-14995955-001',
+    instrument='EUR_USD',
+    units=1
+)
+
+#
+# Process the response
+#
+print("Response: {} ({})".format(response.status, response.reason))
